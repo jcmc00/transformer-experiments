633c08a343ad92bea0c13886e12018616e6d663a
Miso <jcmcdonald96@gmail.com>
2020-02-27 00:06:35 +0800

    Most blocks to modules. Created encoder layer (opennmt)

---
+++ modules.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from itertools import zip_longest
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, epsilon = 1e-8):
+        super().__init__()
+        self.mults = nn.Parameter(torch.ones(features))
+        self.adds = nn.Parameter(torch.zeros(features))
+        self.epsilon = epsilon
+    def forward(self, x):
+        m = x.mean(-1, keepdim = True)
+        v = x.var(-1, keepdim = True)
+        x = x - m / (v + self.epsilon).sqrt()
+        return x*self.mults + self.adds
+
+# Mask future values for 'causality' flag
+def attention(query, key, value, dropout=None):
+    # q,k,v are all of size d_k = d_v
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(d_k)
+
+    # attention weights
+    p_attn = F.softmax(scores, dim = -1)
+    if dropout.p > 0:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, d_model, n_heads, dropout = 0.1):
+        super().__init__()
+        self.d_k = d_model // n_heads
+        self.h = n_heads
+        self.lin = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(p = dropout)
+        self.attn = None
+
+    def forward(self, query, key, value):
+        nbatches = query.shape[0]
+        # broadcast could train on same weights
+        lx = zip_longest([self.lin], (query, key, value), fillvalue = self.lin)  # [(lin,q), (lin,k), (lin, v)] with broadcast
+        q, k, v = [layer(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2) for layer, x in lx]
+
+        x, self.attn = attention(q, k, v, dropout = self.dropout)
+
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        return self.lin(x)
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout = 0.1):
+        super().__init__()
+        self.w1 = nn.Linear(d_model, d_ff)
+        self.w2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
--- transformer.py
+++ transformer.py
@@ -1,74 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from itertools import zip_longest
+from modules import LayerNorm, MultiHeadedAttention, PositionwiseFeedForward
 import copy
 
 # copies don't reference same memory
 def clones(module, n):
-    return nn.ModuleList([copy.deepcoy(module) for _ in range(n)])
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(n)])
 
-class LayerNorm(nn.Module):
-    def __init__(self, features, epsilon = 1e-8):
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, d_model, n_heads, d_ff, dropout = 0.1):
         super().__init__()
-        self.mults = nn.Parameter(torch.ones(features))
-        self.adds = nn.Parameter(torch.zeros(features))
-        self.epsilon = epsilon
-    def forward(self, x):
-        m = x.mean(-1, keepdim = True)
-        v = x.var(-1, keepdim = True)
-        x = x - m / (v + self.epsilon).sqrt()
-        return x*self.mults + self.adds
+        self.self_attn = MultiHeadedAttention(d_model, n_heads, dropout=dropout)
 
-class Encoder(nn.Module):
-    def __init__(self, layer, n):
-        super().__init__()
-        self.layers = clones(layer, n)
-        self.norm = LayerNorm(layer.size)
-        
-    def forward(self, x, mask):
-        for layer in self.layers:
-            x = layer(x, mask)
-        return self.norm(x)
-
-class SublayerConnection(nn.Module):
-    def __init__(self, features, dropout):
-        super().__init__()
-        self.norm = LayerNorm(features)
+        self.feed_forward = PositionwiseFeedForward(d_model, d_ff, dropout)
+        self.layer_norm = LayerNorm(d_model, epsilon=1e-6)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, sublayer):
-        # residual connection
-        return x + self.dropout(sublayer(self.norm(x)))
-
-# Mask future values for 'causality' flag
-def attention(query, key, value, dropout=None):
-    # q,k,v are all of size d_k = d_v
-    d_k = query.size(-1)
-    scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(d_k)
-
-    # attention weights
-    p_attn = F.softmax(scores, dim = -1)
-    if dropout.p > 0:
-        p_attn = dropout(p_attn)
-    return torch.matmul(p_attn, value), p_attn
-
-class MultiHeadedAttention(nn.Module):
-    def __init__(self, d_model, n_heads, dropout = 0):
-        super().__init__()
-        self.d_k = d_model // n_heads
-        self.h = n_heads
-        self.lin = nn.Linear(d_model, d_model)
-        self.dropout = nn.Dropout(p = dropout)
-        self.attn = None
-
-    def forward(self, query, key, value):
-        nbatches = query.shape[0]
-        # broadcast could train on same weights
-        lx = zip_longest([self.lin], (query, key, value), fillvalue = self.lin)  # [(lin,q), (lin,k), (lin, v)] with broadcast
-        q, k, v = [layer(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2) for layer, x in lx]
-
-        x, self.attn = attention(q, k, v, dropout = self.dropout)
-
-        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
-        return self.lin(x)
+    def forward(self, x, mask):
+        input_norm = self.layer_norm(x) # LayerNorm before attn
+        context, _ = self.self_attn(input_norm, input_norm, input_norm, mask=mask)
+        out = self.dropout(context) + x
+        return self.feed_forward(out)
